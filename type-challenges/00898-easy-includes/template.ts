e1c035dec5fc98571d74c53524ff3dc33e144b1d
ziyoub210 <ziyoub210@163.com>
2023-09-19 22:24:33 +0800

    feat: 00898-easy-includes

---
+++ type-challenges/00898-easy-includes/template.ts
@@ -0,0 +1,7 @@
+import type { Equal } from '@type-challenges/utils';
+
+export type Includes<T extends any[], U> = T extends [infer First, ...infer Rest]
+  ? Equal<First, U> extends true
+    ? true
+    : Includes<Rest, U>
+  : false;
